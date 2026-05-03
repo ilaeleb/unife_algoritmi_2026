fcbd85b92274bcd221edeb4ccaf94243030fa0a4
ilaeleb <ilariaebrunetti@gmail.com>
2026-05-03 17:38:27 +0200

    main

--- src/firma_linux.c
+++ src/firma_linux.c
@@ -3,12 +3,14 @@
 #include <stdint.h>
 #include "pattern.h"
 
-// #define INPUT_FILE "input_output/input_firma_big.txt"
-// #define OUTPUT_FILE "input_output/output_firma_big.txt"
+#define INPUT_FILE "input_output/input_firma_big.txt"
+#define OUTPUT_FILE "input_output/output_firma_big.txt"
 
-#define INPUT_FILE "input_output/input_firma_small.txt"
-#define OUTPUT_FILE "input_output/output_firma_small.txt"
+// #define INPUT_FILE "input_output/input_firma_small.txt"
+// #define OUTPUT_FILE "input_output/output_firma_small.txt"
 
+// #define INPUT_FILE "input_output/input_firma_prof.txt"
+// #define OUTPUT_FILE "input_output/output_firma_prof.txt"
 
 // #define INPUT_FILE "input.txt"
 // #define OUTPUT_FILE "output.txt"
@@ -19,6 +21,7 @@ int main() {
     start = __rdtsc(); 
 
     FILE *input = fopen(INPUT_FILE, "r");
+    FILE *output = fopen(OUTPUT_FILE, "w");
 
     if (input == NULL) {
         printf("File not found.\n");
@@ -32,11 +35,24 @@ int main() {
     char *virus = malloc(virus_lenght * sizeof(char));
     fscanf(input, "%s", virus);
 
-    printf("%s\n", virus);
+    uint8_t pattern_lenght;
+    char *pattern = malloc(20 * sizeof(char));;
+    int16_t i, *count = malloc(n_query * sizeof(int16_t));
+    int32_t *index = malloc(1 * sizeof(int32_t));
 
-    delta *dmatrix = compute_delta_matrix("abaabaszdsadsdwpn",17);
+    for (i = 0; i < n_query; i++) {
+        fscanf(input, "%hhu %s", &pattern_lenght, pattern);
+        count[i] = search_pattern(pattern, virus, pattern_lenght, virus_lenght, index);
+        fprintf(output, "%d\n", *index);
+    }
+
+    for (i = 0; i < n_query; i++) {
+        fprintf(output, "%hd\n", count[i]);
+    }
 
     fclose(input);
+    fclose(output);
+    free(index);
 
     end = __rdtsc();   
     double cpu_hz = 2.4e9;   // 2.4 GHz
